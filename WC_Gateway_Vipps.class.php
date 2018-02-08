bed77818a205163f13ec5a4db439d6c68f95a92c
Iver Odin Kvello <iver@ignitas.no>
2018-02-08 17:18:56 +0100

    PSR-2 indenting

--- WC_Gateway_Vipps.class.php
+++ WC_Gateway_Vipps.class.php
@@ -1,80 +1,80 @@
 <?php
 
 class WC_Gateway_Vipps extends WC_Payment_Gateway {
-	public $form_fields = null;
-	public $id = 'vipps';
-	public $icon = ''; // IOK FIXME
-	public $has_fields = false; // IOK FIXME
-	public $method_title = 'Vipps';
-	public $title = 'Vipps';
-	public $method_description = "";
+    public $form_fields = null;
+    public $id = 'vipps';
+    public $icon = ''; // IOK FIXME
+    public $has_fields = false; // IOK FIXME
+    public $method_title = 'Vipps';
+    public $title = 'Vipps';
+    public $method_description = "";
 
-	public function __construct() {
-		$this->method_description = __('Offer Vipps as a payment method', 'vipps');
-		$this->method_title = __('Vipps','vipps');
-		$this->title = __('Vipps','vipps');
-		$this->init_form_fields();
-		$this->init_settings();
-		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
-	}
+    public function __construct() {
+        $this->method_description = __('Offer Vipps as a payment method', 'vipps');
+        $this->method_title = __('Vipps','vipps');
+        $this->title = __('Vipps','vipps');
+        $this->init_form_fields();
+        $this->init_settings();
+        add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+    }
 
-	public function init_form_fields() { 
-		$this->form_fields = array(
-				'enabled' => array(
-					'title'       => __( 'Enable/Disable', 'woocommerce' ),
-					'label'       => __( 'Enable Vipps', 'vipps' ),
-					'type'        => 'checkbox',
-					'description' => '',
-					'default'     => 'no',
-					),
-				'title' => array(
-					'title' => __( 'Title', 'woocommerce' ),
-					'type' => 'text',
-					'description' => __( 'This controls the title which the user sees during checkout.', 'woocommerce' ),
-					'default' => __('Vipps','vipps')
-					),
-				'description' => array(
-					'title' => __( 'Description', 'woocommerce' ),
-					'type' => 'textarea',
-					'description' => __( 'This controls the description which the user sees during checkout.', 'woocommerce' ),
-					'default' => __("Pay with Vipps", 'vipps')
-					)
-				);
-	}
+    public function init_form_fields() { 
+        $this->form_fields = array(
+                'enabled' => array(
+                    'title'       => __( 'Enable/Disable', 'woocommerce' ),
+                    'label'       => __( 'Enable Vipps', 'vipps' ),
+                    'type'        => 'checkbox',
+                    'description' => '',
+                    'default'     => 'no',
+                    ),
+                'title' => array(
+                    'title' => __( 'Title', 'woocommerce' ),
+                    'type' => 'text',
+                    'description' => __( 'This controls the title which the user sees during checkout.', 'woocommerce' ),
+                    'default' => __('Vipps','vipps')
+                    ),
+                'description' => array(
+                    'title' => __( 'Description', 'woocommerce' ),
+                    'type' => 'textarea',
+                    'description' => __( 'This controls the description which the user sees during checkout.', 'woocommerce' ),
+                    'default' => __("Pay with Vipps", 'vipps')
+                    )
+                );
+    }
 
-	// IOK FIXME  the core of things
-	public function process_payment ($order_id) {
-		global $woocommerce;
-		$order = new WC_Order( $order_id );
+    // IOK FIXME  the core of things
+    public function process_payment ($order_id) {
+        global $woocommerce;
+        $order = new WC_Order( $order_id );
 
-		// Mark as on-hold (we're awaiting the cheque)
-		$order->update_status('on-hold', __( 'Vipps payment initiated', 'vipps' ));
+        // Mark as on-hold (we're awaiting the cheque)
+        $order->update_status('on-hold', __( 'Vipps payment initiated', 'vipps' ));
 
-		// Reduce stock levels
-		$order->reduce_order_stock();
+        // Reduce stock levels
+        $order->reduce_order_stock();
 
-		// Remove cart
-		$woocommerce->cart->empty_cart();
+        // Remove cart
+        $woocommerce->cart->empty_cart();
 
-		// Return thankyou redirect
-		return array(
-				'result' => 'success',
-				'redirect' => $this->get_return_url( $order )
-			    );
-	}
+        // Return thankyou redirect
+        return array(
+                'result' => 'success',
+                'redirect' => $this->get_return_url( $order )
+                );
+    }
 
-	public function admin_options() {
-		?>
-			<h2><?php _e('Vipps','vipps'); ?></h2>
-			<table class="form-table">
-			<?php $this->generate_settings_html(); ?>
-			</table> <?php
-	}
+    public function admin_options() {
+        ?>
+            <h2><?php _e('Vipps','vipps'); ?></h2>
+            <table class="form-table">
+            <?php $this->generate_settings_html(); ?>
+            </table> <?php
+    }
 
-	function process_admin_options () {
-		// Handle options updates
-		$saved = parent::process_admin_options();
-		return $saved;
-	}
+    function process_admin_options () {
+        // Handle options updates
+        $saved = parent::process_admin_options();
+        return $saved;
+    }
 
 }
--- index.php
+++ index.php
@@ -9,36 +9,36 @@
 /* This class is for hooks and plugin managent, and is instantiated as a singleton. IOK 2018-02-07*/
 class Vipps {
 
-	function __construct() {
-	}
+    function __construct() {
+    }
 
-	public function admin_init () {
-	}
+    public function admin_init () {
+    }
 
-	public function admin_menu () {
-	}
+    public function admin_menu () {
+    }
 
-	public function init () {
-	}
+    public function init () {
+    }
 
-	public function plugins_loaded() {
-                /* The gateway is added at 'plugins_loaded' and instantiated by Woo itself. IOK 2018-02-07 */
-		require_once(dirname(__FILE__) . "/WC_Gateway_Vipps.class.php");
-		add_filter( 'woocommerce_payment_gateways', array($this,'woocommerce_payment_gateways' ));
-	}
-	/* WooCommerce Hooks */
-	public function woocommerce_payment_gateways($methods) {
-		$methods[] = 'WC_Gateway_Vipps'; 
-		return $methods;
-	}
-        /* End Woocommerce hoos*/
+    public function plugins_loaded() {
+        /* The gateway is added at 'plugins_loaded' and instantiated by Woo itself. IOK 2018-02-07 */
+        require_once(dirname(__FILE__) . "/WC_Gateway_Vipps.class.php");
+        add_filter( 'woocommerce_payment_gateways', array($this,'woocommerce_payment_gateways' ));
+    }
+    /* WooCommerce Hooks */
+    public function woocommerce_payment_gateways($methods) {
+        $methods[] = 'WC_Gateway_Vipps'; 
+        return $methods;
+    }
+    /* End Woocommerce hoos*/
 
-	public function activate () {
-	}
-	public function uninstall() {
-	}
-	public function footer() {
-	}
+    public function activate () {
+    }
+    public function uninstall() {
+    }
+    public function footer() {
+    }
 }
 
 /* Instantiate the singleton, stash it in a global and add hooks. IOK 2018-02-07 */
@@ -48,10 +48,10 @@ register_activation_hook(__FILE__,array($Vipps,'activate'));
 register_uninstall_hook(__FILE__,array($Vipps,'uninstall'));
 
 if (is_admin()) {
-	add_action('admin_init',array($Vipps,'admin_init'));
-	add_action('admin_menu',array($Vipps,'admin_menu'));
+    add_action('admin_init',array($Vipps,'admin_init'));
+    add_action('admin_menu',array($Vipps,'admin_menu'));
 } else {
-	add_action('wp_footer', array($Vipps,'footer'));
+    add_action('wp_footer', array($Vipps,'footer'));
 }
 add_action('init',array($Vipps,'init'));
 add_action( 'plugins_loaded', array($Vipps,'plugins_loaded'));
