dfc3d669b05b397186eeacf9f07b5783d8afd423
Iver Odin Kvello <iver@ignitas.no>
2018-05-04 17:40:16 +0200

    Small refactor

---
+++ Vipps.class.php
@@ -0,0 +1,264 @@
+<?php
+/*
+   Plugin Name: Woocommerce Vipps Payment Module
+   Description: Offer Vips as a payment method for Woocommerce
+   Author: Iver Odin Kvello
+   Version: 0.9
+ */
+require_once(dirname(__FILE__) . "/exceptions.php");
+
+/* This class is for hooks and plugin managent, and is instantiated as a singleton. IOK 2018-02-07*/
+class Vipps {
+
+    function __construct() {
+    }
+
+    public function admin_init () {
+    }
+
+    public function admin_menu () {
+    }
+
+    public function init () {
+    }
+
+    public function log ($what,$type='info') {
+        $logger = wc_get_logger();
+        $context = array('source','Vipps Woo Gateway');
+        $logger->log($type,$what,$context);
+    }
+
+
+    // Because the prefix used to create the Vipps order id is editable
+    // by the user, we will store that as a meta and use this for callbacks etc.
+    public function getOrderIdByVippsOrderId($vippsorderid) {
+        global $wpdb;
+        return $wpdb->get_var( $wpdb->prepare( "SELECT post_id FROM {$wpdb->prefix}postmeta WHERE meta_key = '_vipps_orderid' AND meta_value = %s", $vippsorderid) );
+    }
+
+    // Temporary redirect handler! IOK FIXME REPLACE IOK 2018-04-23
+    // This needs to be an actual page instead, which must be created on plugin activate
+    // and then selected, and error-handling added and so forth.
+    public function template_redirect() {
+        // Check if using pretty links, if so, use the pretty link, otherwise use a GET parameter which we will need to add, ala VFlow=orderid
+        $isvippscheckout = 0;
+        $orderid=0;
+        if ( get_option('permalink_structure')) {
+            if (preg_match("!^/vipps-betaling/([^/]*)!", $_SERVER['REQUEST_URI'], $matches)) { 
+                $isvippscheckout = 1;
+            }
+        } else {
+            if (isset($_GET['VippsBetaling'])) {
+                $isvippscheckout = 1;
+            }
+        } 
+
+        if ($isvippscheckout) {
+            // Call a method here in the gatway here IOK FIXME
+            status_header(200,'OK');
+            $orderid = WC()->session->get('_vipps_pending_order');
+            $order = null;
+            if ($orderid) {
+                $order = new WC_Order($orderid); 
+            }
+            // Check that order exists and belongs to our session. Can use WC()->session->get() I guess - set the orderid or a hash value in the session
+            // and check that the order matches (and is 'pending') (and exists)
+            $transid = $order->get_meta('_vipps_transaction');
+            $vippsstamp = $order->get_meta('_vipps_init_timestamp');
+            $vippsstatus = $order->get_meta('_vipps_init_status');
+            $message = $order->get_meta('_vipps_confirm_message');
+
+            $content = "<pre>This is where we await user confirmation:\n";
+            $content .= htmlspecialchars("$message\n$vippsstatus\n" . date('Y-m-d H:i:s',$vippsstamp));
+
+            $this->fakepage(__('Confirm your purchase in your Vipps app','vipps'), $content);
+        }
+    }
+
+    public function plugins_loaded() {
+        /* The gateway is added at 'plugins_loaded' and instantiated by Woo itself. IOK 2018-02-07 */
+        require_once(dirname(__FILE__) . "/WC_Gateway_Vipps.class.php");
+        add_filter( 'woocommerce_payment_gateways', array($this,'woocommerce_payment_gateways' ));
+        add_action( 'woocommerce_api_wc_gateway_vipps', array($this,'vipps_callback'));
+
+        // Special pages and callbacks handled by template_redirect
+        add_action('template_redirect', array($this,'template_redirect'));
+
+        // Ajax endpoints for checking the order status while waiting for confirmation
+        add_action('wp_ajax_nopriv_check_order_status', array($this, 'ajax_check_order_status'));
+        add_action('wp_ajax_check_order_status', array($this, 'ajax_check_order_status'));
+
+    }
+
+    // This is the main callback from Vipps when payments are returned. IOK 2018-04-20
+    public function vipps_callback() {
+        $raw_post = @file_get_contents( 'php://input' );
+        $result = @json_decode($raw_post,true);
+        if (!$result) {
+            $this->log(__("Did not understand callback from Vipps:",'vipps') . " " .  $raw_post);
+            return false;
+        }
+        require_once(dirname(__FILE__) . "/WC_Gateway_Vipps.class.php"); // Unneccessary, as this will have been initialized by Woo, but anyway . 2018-04-27
+        $gw = new WC_Gateway_Vipps();
+        return $gw->handle_callback($result);
+    }
+
+    /* WooCommerce Hooks */
+    public function woocommerce_payment_gateways($methods) {
+        $methods[] = 'WC_Gateway_Vipps'; 
+        return $methods;
+    }
+    /* End Woocommerce hoos*/
+
+    public function activate () {
+
+    }
+    public function uninstall() {
+    }
+    public function footer() {
+    }
+ 
+    public function wha () {
+      print "hwa";
+    }
+
+    // Check order status in the database, and if it is on-hold for a long time, directly at Vipps
+    // IOK 2018-05-04
+    public function check_order_status($order) {
+      if (!$order) return null;
+      $order_status = $order->get_status();
+      if ($order_status != 'on-hold') return $order_status;
+      // No callback has occured yet. If this has been going on for a while, check directly with Vipps
+      if ($order_status == 'on-hold') {
+            $now = time();
+            $then= $order->get_meta('_vipps_init_timestamp');
+            if ($then + (1 * 60) < $now) { // more than a minute? Start checking at Vipps
+               return $order_status;
+            }
+      }
+      $this->log("Checking order status on Vipps");
+      require_once(dirname(__FILE__) . "/WC_Gateway_Vipps.class.php");
+      $gw = new WC_Gateway_Vipps();
+      try {
+       $order_status = $gw->calback_check_order_status($order);
+       return $order_status;
+      } catch (Exception $e) {
+        $this->log($e->getMessage());
+        return null;
+      }
+    }
+
+    // Check the status of the order if it is a part of our session, and return a result to the handler function IOK 2018-05-04
+    public function ajax_check_order_status () {
+        check_ajax_referer('vippsstatus','sec');
+
+        $orderid= wc_get_order_id_by_order_key($_POST['key']);
+        $transaction = wc_get_order_id_by_order_key($_POST['transaction']);
+
+        $sessionorders= WC()->session->get('_vipps_session_orders');
+        if (!isset($sessionorders[$orderid])) {
+            $this->log(__('The orderid passed is not from this session:','vipps') . $orderid);
+            wp_send_json(array('status'=>'error', 'msg'=>__('Not an order','vipps')));
+        }
+
+        $order = new WC_Order($orderid); 
+        if (!$order) {
+            wp_send_json(array('status'=>'error', 'msg'=>__('Not an order','vipps')));
+        }
+
+        $order_status = $this->callback_check_order_status($order);
+        if ($order_status == 'processing') {
+            wp_send_json(array('status'=>'ok', 'msg'=>__('Payment reserved', 'vipps')));
+        }
+        if ($order_status == 'completed') {
+            wp_send_json(array('status'=>'ok', 'msg'=>__('Order complete', 'vipps')));
+        }
+
+        if ($order_status == 'failed') {
+            wp_send_json(array('status'=>'fail', 'msg'=>__('Order cancelled', 'vipps')));
+        }
+
+        // No callback has occured yet. If this has been going on for a while, check directly with Vipps
+        if ($order_status == 'on-hold') {
+            wp_send_json(array('status'=>'waiting', 'msg'=>__('Waiting on confirmation', 'vipps')));
+        }
+        wp_send_json(array('status'=>'error', 'msg'=>__('Unknown order','vipps')));
+        return false;
+    }
+
+    public function fakepage($title,$content) {
+        global $wp, $wp_query;
+        $post = new stdClass();
+        $post->ID = -99;
+        $post->post_author = 1;
+        $post->post_date = current_time( 'mysql' );
+        $post->post_date_gmt = current_time( 'mysql', 1 );
+        $post->post_title = $title;
+        $post->post_content = $content;
+        $post->post_status = 'publish';
+        $post->comment_status = 'closed';
+        $post->ping_status = 'closed';
+        $post->post_name = 'vippsconfirm-fake-page-name';
+        $post->post_type = 'page';
+        $post->filter = 'raw'; // important
+        $wp_post = new WP_Post($post);
+        wp_cache_add( -99, $wp_post, 'posts' );
+        // Update the main query
+        $wp_query->post = $wp_post;
+        $wp_query->posts = array( $wp_post );
+        $wp_query->queried_object = $wp_post;
+        $wp_query->queried_object_id = $post_id;
+        $wp_query->found_posts = 1;
+        $wp_query->post_count = 1;
+        $wp_query->max_num_pages = 1; 
+        $wp_query->is_page = true;
+        $wp_query->is_singular = true; 
+        $wp_query->is_single = false; 
+        $wp_query->is_attachment = false;
+        $wp_query->is_archive = false; 
+        $wp_query->is_category = false;
+        $wp_query->is_tag = false; 
+        $wp_query->is_tax = false;
+        $wp_query->is_author = false;
+        $wp_query->is_date = false;
+        $wp_query->is_year = false;
+        $wp_query->is_month = false;
+        $wp_query->is_day = false;
+        $wp_query->is_time = false;
+        $wp_query->is_search = false;
+        $wp_query->is_feed = false;
+        $wp_query->is_comment_feed = false;
+        $wp_query->is_trackback = false;
+        $wp_query->is_home = false;
+        $wp_query->is_embed = false;
+        $wp_query->is_404 = false; 
+        $wp_query->is_paged = false;
+        $wp_query->is_admin = false; 
+        $wp_query->is_preview = false; 
+        $wp_query->is_robots = false; 
+        $wp_query->is_posts_page = false;
+        $wp_query->is_post_type_archive = false;
+        // Update globals
+        $GLOBALS['wp_query'] = $wp_query;
+        $wp->register_globals();
+        return $wp_post;
+    }
+
+}
+
+/* Instantiate the singleton, stash it in a global and add hooks. IOK 2018-02-07 */
+global $Vipps;
+$Vipps = new Vipps();
+register_activation_hook(__FILE__,array($Vipps,'activate'));
+register_uninstall_hook(__FILE__,array($Vipps,'uninstall'));
+
+if (is_admin()) {
+    add_action('admin_init',array($Vipps,'admin_init'));
+    add_action('admin_menu',array($Vipps,'admin_menu'));
+} else {
+    add_action('wp_footer', array($Vipps,'footer'));
+}
+add_action('init',array($Vipps,'init'));
+add_action( 'plugins_loaded', array($Vipps,'plugins_loaded'));
+
+?>
