c7f70485536c505e41ca3869d6c9af6dfcc5f7fa
Iver Odin Kvello <iver@ignitas.no>
2018-08-06 15:07:08 +0200

    No need

--- WC_Gateway_Vipps.class.php
+++ WC_Gateway_Vipps.class.php
@@ -1027,7 +1027,6 @@ class WC_Gateway_Vipps extends WC_Payment_Gateway {
         // Use Billing Phone if it is required, otherwise ask for a phone IOK 2018-04-24
         // For v2 of the api, just let Vipps ask for then umber
 	print $this->get_option('description');
-	parent::payment_fields();
         return;
         if (isset($fields['billing']['billing_phone']) && $fields['billing']['billing_phone']['required']) {
         } else {
