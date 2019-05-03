47b86b645a698ea7de1412c76393def0836898b8
Iver Odin Kvello <iver@ignitas.no>
2019-05-03 09:28:24 +0200

    Don't show empty Vipps metabox for non-vipps orders

--- Vipps.class.php
+++ Vipps.class.php
@@ -135,7 +135,14 @@ class Vipps {
 
     public function add_meta_boxes () {
         // Metabox showing order status at Vipps IOK 2018-05-07
-        add_meta_box( 'vippsdata', __('Vipps','woo-vipps'), array($this,'add_vipps_metabox'), 'shop_order', 'side', 'core' );
+        global $post;
+        if ($post && get_post_type($post) == 'shop_order' ) {
+           $order = wc_get_order($post);
+           $pm = $order->get_payment_method();
+           if ($pm == 'vipps') { 
+             add_meta_box( 'vippsdata', __('Vipps','woo-vipps'), array($this,'add_vipps_metabox'), 'shop_order', 'side', 'core' );
+           }
+        }
     }
 
     public function wp_enqueue_scripts() {
