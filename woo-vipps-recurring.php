d361b58a6cd0221f348752d5a5b66b9e73f34752
Marcuz <marcuz1111@gmail.com>
2020-03-18 13:44:09 +0100

    WC  tested up to: 4.0.0, WP tested up to: 5.4.0

--- woo-vipps-recurring.php
+++ woo-vipps-recurring.php
@@ -7,8 +7,8 @@
  * Author URI: https://vipps.no
  * Version: 1.0.5
  * Requires at least: 4.4
- * Tested up to: 5.3.2
- * WC requires at least: 5.0.0
+ * Tested up to: 5.4.0
+ * WC tested up to: 4.0.0
  * Text Domain: woo-vipps-recurring
  * Domain Path: /languages
  */
