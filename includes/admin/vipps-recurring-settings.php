9cdb7bf753f1654254827d1d5e90a3bf4ef51862
Marcuz <marcuz1111@gmail.com>
2020-05-27 16:04:04 +0200

    default pending renewal status should be on-hold as before

--- includes/admin/vipps-recurring-settings.php
+++ includes/admin/vipps-recurring-settings.php
@@ -62,7 +62,7 @@ return apply_filters(
 			'type'        => 'select',
 			'title'       => __( 'Default status to give pending renewals', 'woo-vipps-recurring' ),
 			'description' => __( 'When a renewal happens we have to wait a few days for the money to be drawn from the customer. You might want such orders to be on hold, or maybe you require them to be processing or completed. A failed charge will still become "failed" regardless of this setting.', 'woo-vipps-recurring' ),
-			'default'     => 'wc-processing',
+			'default'     => 'wc-on-hold',
 			'options'     => array_filter( wc_get_order_statuses(), static function ( $key ) {
 				return in_array( $key, [ 'wc-processing', 'wc-on-hold', 'wc-completed' ] );
 			}, ARRAY_FILTER_USE_KEY )
