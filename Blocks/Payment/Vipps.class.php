8dd4cb5b7ff06cebc94e3f8cf59646ce691c49a9
Iver Odin Kvello <iver@wp-hosting.no>
2020-08-12 14:43:09 +0200

    after feedback

--- Blocks/Payment/Vipps.class.php
+++ Blocks/Payment/Vipps.class.php
@@ -8,33 +8,21 @@
 
 namespace Automattic\WooCommerce\Blocks\Payments\Integrations;
 
-use Automattic\WooCommerce\Blocks\Assets\Api as AssetApi;
 use Automattic\WooCommerce\Blocks\Package;
-
 
 final class Vipps extends AbstractPaymentMethodType {
         private $localized=0;
 	protected $name = 'vipps';
-	private $asset_api;
-	public function __construct( AssetApi $asset_api ) {
-		$this->asset_api = $asset_api;
-	}
+
 	public function initialize() {
 		$this->settings = get_option( 'woocommerce_vipps_settings', [] );
 	}
 
         // Register this payment method IOK 2020-08-10
         public static function register() {
-            $container = Package::container();
-            $container->register(Vipps::class,
-                        function( $container ) {
-                                $asset_api = $container->get(AssetApi::class); 
-                                return new Vipps($asset_api);
-                        });
             add_action( 'woocommerce_blocks_payment_method_type_registration', 
                         function ($registry) {
-                            $container = Package::container();
-                            $registry->register($container->get(Vipps::class ));
+                            $registry->register(new static());
             });
         }
 
@@ -43,13 +31,13 @@ final class Vipps extends AbstractPaymentMethodType {
 	}
 	public function get_payment_method_script_handles() {
 
-                // Cannot call the asset_api's register_script method because it takes a relative url, so we need to replicate
-                // its functionality, in particular the dependencies. IOK 2020-08-10
-                $dependencies = array('wc-vendors', 'wc-blocks' );
+                $version = filemtime(dirname(__FILE__) . "/js/wc-payment-method-vipps.js");
+                $path = plugins_url('js/wc-payment-method-vipps.js', __FILE__);
                 $handle = 'wc-payment-method-vipps';
-                wp_register_script($handle, plugins_url('js/wc-payment-method-vipps.js', __FILE__), 
-                                    apply_filters( 'woocommerce_blocks_register_script_dependencies', $dependencies, $handle ),
-                                    filemtime(dirname(__FILE__) . "/js/wc-payment-method-vipps.js"),true);
+                $dependencies = array('wc-vendors', 'wc-blocks' );
+                $dependencies = apply_filters('woocommerce_blocks_register_script_dependencies', $dependencies, $handle );
+
+                wp_register_script($handle, $path, $dependencies,$version,true);
 
 
                 // Will not use wp_set_script_translations yet, it seems to be not fully compatible with Loco Translate etc. Instead use
