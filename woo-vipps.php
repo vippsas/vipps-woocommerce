e13c89c47fe1d417b564b1f52d072c95049e4616
Iver Odin Kvello <iver@wp-hosting.no>
2021-08-06 14:25:08 +0200

    Testing

--- woo-vipps.php
+++ woo-vipps.php
@@ -7,14 +7,14 @@
    Author URI: https://www.wp-hosting.no/
    Text-domain: woo-vipps
    Domain Path: /languages
-   Version: 1.7.10
+   Version: 1.7.11
 
    Requires at least: 4.7
-   Tested up to: 5.7.2
+   Tested up to: 5.8.0
    Stable tag: trunk
    Requires PHP: 5.6
    WC requires at least: 3.3.4
-   WC tested up to: 5.4.1
+   WC tested up to: 5.5.2
 
    License: MIT
    License URI: https://choosealicense.com/licenses/mit/
@@ -49,7 +49,7 @@ SOFTWARE.
 
 
 // Report version externally
-define('WOO_VIPPS_VERSION', '1.7.10');
+define('WOO_VIPPS_VERSION', '1.7.11');
 
 if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
