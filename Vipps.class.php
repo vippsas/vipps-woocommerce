7537810d309604d93b09ccfbf523506569ffa0b5
Iver Odin Kvello <iver@wp-hosting.no>
2021-08-11 14:56:29 +0200

    done

--- Vipps.class.php
+++ Vipps.class.php
@@ -366,8 +366,7 @@ class Vipps {
         add_shortcode('woo_vipps_express_checkout_button', array($this, 'express_checkout_button_shortcode'));
         add_shortcode('woo_vipps_express_checkout_banner', array($this, 'express_checkout_banner_shortcode'));
         // The Vipps Checkout feature which overrides the normal checkout process.
-// IOK FIXME TEMPORARILY DISABLED FOR THE SUMMER
-//        add_shortcode('vipps_checkout', array($this, 'vipps_checkout_shortcode'));
+        add_shortcode('vipps_checkout', array($this, 'vipps_checkout_shortcode'));
     }
 
     public function log ($what,$type='info') {
