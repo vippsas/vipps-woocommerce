460ab4d0bfa610ebd7deb663728848ce42e30714
Iver Odin Kvello <iver@wp-hosting.no>
2021-08-18 09:38:19 +0200

    Versions

--- woo-vipps.php
+++ woo-vipps.php
@@ -7,7 +7,7 @@
    Author URI: https://www.wp-hosting.no/
    Text-domain: woo-vipps
    Domain Path: /languages
-   Version: 1.7.10
+   Version: 1.8.0
 
    Requires at least: 4.7
    Tested up to: 5.7.2
@@ -49,7 +49,7 @@ SOFTWARE.
 
 
 // Report version externally
-define('WOO_VIPPS_VERSION', '1.7.10');
+define('WOO_VIPPS_VERSION', '1.8.0');
 
 if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
