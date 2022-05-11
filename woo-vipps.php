437d0c473cdf5a7b57fbc7c716fb25c261e19bb1
Iver Odin Kvello <iver@wp-hosting.no>
2022-05-11 13:19:38 +0200

    Testing for 6.0

--- woo-vipps.php
+++ woo-vipps.php
@@ -10,7 +10,7 @@
    Version: 1.8.21
    Stable tag: 1.8.21
    Requires at least: 4.7
-   Tested up to: 5.9.3
+   Tested up to: 6.0.0
    Requires PHP: 5.6
    WC requires at least: 3.3.4
    WC tested up to: 6.4.1
