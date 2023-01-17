a3b52cdfc01989ee9b0f6f27a518403c7f2a5a7c
Iver Odin Kvello <iver@wp-hosting.no>
2023-01-17 13:10:44 +0100

    inheritance

--- shipping-method-example.php
+++ shipping-method-example.php
@@ -9,56 +9,30 @@ add_action( 'woocommerce_shipping_init', function () {
   });
 
 
-/* Abstract method used just to mark the special Vipps Checkout shipping methods */
+/* Abstract class used just to mark the special Vipps Checkout shipping methods. Don't instantiate this */
 class VippsCheckout_Shipping_Method extends WC_Shipping_Method {
+    public $defaulttitle = "";
+    // Basic price, set by options
+    public $cost = 0;
 
-}
-
-class VippsCheckout_Shipping_Method_Posten extends VippsCheckout_Shipping_Method {
-
-    public function __construct( $instance_id = 0 ) {
-        $this->instance_id 	  = absint( $instance_id );
-        $this->id                 = 'vipps_checkout_posten';
-
-        // in process_admin_options, endre denne til korrekt tittel med prefiks "Vipps Checkout"
-        
-        $this->method_title       = __( 'Vipps Checkout: Posten', 'woo-vipps' );
-        $this->method_description = __( 'Fraktmetode spesielt for Vipps Checkout: Posten Norge', 'woo-vipps' );
-        //add to shipping zones list
-        $this->supports = array(
-                'shipping-zones', 
-                'instance-settings',
-                'instance-settings-modal',
-                );
-        $this->title = __( 'Vipps Checkout: Posten', 'woo-vipps' );
-        $this->init();
+    public function preinit () {
+    }
+    public function init () {
     }
 
-    public function is_enabled() {
-        // Only show when Vipps Checkout is active FIXME (and maybe the settings page)
-        return true;
+    public function instance_form_fields() {
+       return [];
     }
 
-    function init() {
-        // Load the settings API
-        $this->init_form_fields();
-        $this->init_settings();
-
-        // Save settings in admin if you have any defined
-        add_action( 'woocommerce_update_options_shipping_' . $this->id, array( $this, 'process_admin_options' ) );
-    }
-
-    // Fields for the settings of the instance
+    // Instance setting, common for all submethods
     function init_form_fields() {
-
-        //fileds for the modal form from the Zones window
         $this->instance_form_fields = array(
 
                 'title' => array(
                     'title' => __( 'Title', 'woo-vipps' ),
                     'type' => 'text',
                     'description' => __( 'Title to be display on site', 'woo-vipps' ),
-                    'default' => __( 'Posten Norge', 'woo-vipps' )
+                    'default' => $this->defaulttitle
                     ),
 
                 'cost' => array(
@@ -69,20 +43,72 @@ class VippsCheckout_Shipping_Method_Posten extends VippsCheckout_Shipping_Method
                     ),
 
                 );
-        // No global settings for this method.
+        foreach($this->instance_form_fields() as $key=>$setting) {
+            $this->instance_form_fields[$key] = $setting;
+        }
+    }
+
+    public function __construct( $instance_id = 0 ) {
+        $this->instance_id 	  = absint( $instance_id );
+
+        //add to shipping zones list
+        $this->supports = array(
+                'shipping-zones', 
+                'instance-settings',
+                'instance-settings-modal',
+                );
+
+        $this->preinit();
+        // Load the settings API
+        $this->init_form_fields();
+        $this->init_settings();
+
+        $this->title                = $this->get_option( 'title' );
+        $this->tax_status           = 'taxable'; //$this->get_option( 'tax_status' );
+        $this->cost                 = $this->get_option( 'cost' );
+
+        // see above - fix this somehow
+        $this->enabled = "yes";
+
+        $this->init();
+
+        add_action( 'woocommerce_update_options_shipping_' . $this->id, array( $this, 'process_admin_options' ) );
+    }
+
+
+}
+
+class VippsCheckout_Shipping_Method_Posten extends VippsCheckout_Shipping_Method {
+    public $id = 'vipps_checkout_posten';
+
+    public function instance_form_fields() {
+         $settings = array();
+         return $settings;
+    }
+
+    public function is_enabled() {
+        // Only show when Vipps Checkout is active FIXME (and maybe the settings page)
+        return true;
+    }
+
+    // Called by the parent constructor before loading settings
+    function preinit() {
+        $this->defaulttitle = __( 'Posten Norge', 'woo-vipps' );
+        $this->method_title = __( 'Vipps Checkout: Posten', 'woo-vipps' );
+        $this->method_description = __( 'Fraktmetode spesielt for Vipps Checkout: Posten Norge', 'woo-vipps' );
+    }
+    function init() {
     }
 
     // Add free shipping trigger etc?
     public function calculate_shipping( $package = array()) {
-        //as we are using instances for the cost and the title we need to take those values drom the instance_settings
-        $intance_settings =  $this->instance_settings;
+        $instance_settings =  $this->instance_settings;
         // Register the rate
         $this->add_rate( array(
                     'id'      => $this->id,
-                    'label' => __("Vipps Checkout: Posten", 'woo-vipps'), // FIXME
                     'cost' => 1,
-                    'label'   => $intance_settings['title'],
-                    'cost'    => $intance_settings['cost'],
+                    'label'   => $instance_settings['title'],
+                    'cost'    => $instance_settings['cost'],
                     'package' => $package,
                     'taxes'   => true,
                     )
