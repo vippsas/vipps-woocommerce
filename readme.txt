2a610cf0eeb306b6e108bb47e52710f17b35c757
Iver Odin Kvello <iver@wp-hosting.no>
2023-02-13 09:34:40 +0100

    feb 14 version of woo

--- readme.txt
+++ readme.txt
@@ -7,7 +7,7 @@ Requires at least: 4.7
 Tested up to: 6.1.1
 Requires PHP: 5.6
 WC requires at least: 3.3.4
-WC tested up to: 7.3
+WC tested up to: 7.4
 License: MIT
 License URI: https://choosealicense.com/licenses/mit/
 
--- woo-vipps.php
+++ woo-vipps.php
@@ -13,7 +13,7 @@
    Tested up to: 6.1.1
    Requires PHP: 5.6
    WC requires at least: 3.3.4
-   WC tested up to: 7.3
+   WC tested up to: 7.4
 
    License: MIT
    License URI: https://choosealicense.com/licenses/mit/
