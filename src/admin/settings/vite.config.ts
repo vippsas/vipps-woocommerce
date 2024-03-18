35682ff6d20fd158c9347fbd63041fcde0584be5
Iver Odin Kvello <iver@wp-hosting.no>
2024-03-18 13:24:15 +0100

    Use WPs react

--- src/admin/settings/vite.config.ts
+++ src/admin/settings/vite.config.ts
@@ -3,10 +3,19 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+      // Disable automatic runtime injection 
+      jsxRuntime: 'classic', 
+    })],
   build: {
     rollupOptions: {
+      external: ['react', 'react-dom'],
       output: {
+        format: "iife", // or "umd"
+        globals: {      // Ensure we can load react from WP
+          react: 'wp.element',
+          'react-dom': 'wp.element',
+        },
         dir: '../../../admin/settings/dist',
         // Necessary to have a consistent output path, so it's easy to reference it when we call wp_enqueue_script().
         entryFileNames: 'plugin.js',
