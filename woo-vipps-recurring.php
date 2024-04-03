738995f34e000e61add2b257f23a856d97c4348e
Marcus Dahl <marcus.dahl@everyday.no>
2024-04-03 13:45:57 +0200

    chore:  update required plugins so it only includes plugins available on wordpress.org

--- woo-vipps-recurring.php
+++ woo-vipps-recurring.php
@@ -6,7 +6,7 @@
  * Author: Everyday AS
  * Author URI: https://everyday.no
  * Version: 1.20.2
- * Requires Plugins: woocommerce, woocommerce-subscriptions
+ * Requires Plugins: woocommerce
  * Requires at least: 6.1
  * Tested up to: 6.5
  * WC tested up to: 8.6
