b5bb20c015ca949db9be58f238dac62a41a85eb0
Marcus Dahl <marcus.dahl@everyday.no>
2024-05-30 16:07:21 +0200

    fix: HPOS after refactor

--- includes/wc-vipps-recurring.php
+++ includes/wc-vipps-recurring.php
@@ -106,7 +106,7 @@ class WC_Vipps_Recurring {
 		// Declare compatibility with WooCommerce HPOS
 		add_action( 'before_woocommerce_init', function () {
 			if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
-				\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__ );
+				\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', WC_VIPPS_RECURRING_MAIN_FILE );
 			}
 		} );
 
