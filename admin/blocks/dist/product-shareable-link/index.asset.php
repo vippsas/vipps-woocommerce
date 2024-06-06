cdcb1a8841245fb6b9e8d4469d9f06bfd89cca76
Iver Odin Kvello <iver@wp-hosting.no>
2024-06-06 14:27:09 +0200

    Fix 'cannot find module ajv/dit/compile/codegen'

--- admin/blocks/dist/product-shareable-link/index.asset.php
+++ admin/blocks/dist/product-shareable-link/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-block-templates', 'wc-components', 'wc-product-editor', 'wp-components', 'wp-i18n'), 'version' => 'f1c644ff6df90a771264');
+<?php return array('dependencies' => array('react', 'wc-block-templates', 'wc-components', 'wc-product-editor', 'wp-components', 'wp-i18n'), 'version' => '14a3d2ff48bd1716dc54');
