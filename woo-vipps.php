10cb97f526907e961d72cae63705c157ed93003b
Iver Odin Kvello <iver@wp-hosting.no>
2024-09-27 15:30:43 +0200

    Modularize

--- woo-vipps.php
+++ woo-vipps.php
@@ -7,8 +7,8 @@
    Author URI: https://www.wp-hosting.no/
    Text-domain: woo-vipps
    Domain Path: /languages
-   Version: 3.0.0
-   Stable tag: 3.0.0
+   Version: 4.0.0
+   Stable tag: 4.0.0
    Requires at least: 6.2
    Tested up to: 6.6.2
    Requires PHP: 7.0
@@ -47,17 +47,8 @@ SOFTWARE.
 
  */
 
-
-// Report version externally
-define('WOO_VIPPS_VERSION', '3.0.0');
-
 if ( ! defined( 'ABSPATH' ) ) {
     exit; // Exit if accessed directly
-}
-
-// Legacy way of starting test mode - please use developer- and test-modes from now on. IOK 2019-08-30
-if ( ! defined('VIPPS_TEST_MODE' )) {
-    define('VIPPS_TEST_MODE', false);
 }
 
 // Only be active if Woocommerce is active, either on site or network activated IOK 2018-08-29
@@ -68,75 +59,11 @@ if ($activesiteplugins) {
 }
 
 if ( in_array( 'woocommerce/woocommerce.php', $activeplugins) ) {
-    /* Instantiate the singleton, stash it in a global and add hooks. IOK 2018-02-07 */
-    require_once(dirname(__FILE__) . '/payment/Vipps.class.php');
-    global $Vipps;
-    $Vipps = Vipps::instance();
-    Vipps::register_hooks();
+    /* Load support for the basic payment plugin IOK 2024-09-27 */
+    require_once(dirname(__FILE__) ."/payment/payment.php");
 
-    /* The QR Code functionality is in its own class for modularity reasons. It is a singleton too. */
-    require_once(dirname(__FILE__) . '/payment/VippsQRCodeController.class.php');
-    VippsQRCodeController::register_hooks();
-
-    /* If Vipps Checkout is activated, load its support. It can still be turned on and off. */
-    if (get_option('woo_vipps_checkout_activated', false)) {
-        require_once(dirname(__FILE__) . '/payment/VippsCheckout.class.php');
-        VippsCheckout::register_hooks();
-    }
-
-    // Gutenberg block for on-site messaging badges, if Gutenberg is installed. IOK 2022-11-16
-    require_once(dirname(__FILE__) . '/payment/Blocks/Badges/vipps-badge.php');
-
-    // Helper code for specific plugins, themes etc
-    require_once(dirname(__FILE__) . '/payment/woo-vipps-compatibility.php');
-
-    // Load code for the new WooCommerce product editor
-    add_action('woocommerce_init', function() {
-        // Only load if we're on a version of WooCommerce that supports all the blocks and features we're using.
-        $is_version_supported = version_compare(wc()->version, '8.6.0', '>=');
-        // Only load if the feature flag is enabled.
-        $is_product_editor_v2_enabled = get_option('woocommerce_feature_product_block_editor_enabled');
-        if($is_version_supported && $is_product_editor_v2_enabled) {
-            // Load the new blocks
-            require_once(dirname(__FILE__) . '/payment/admin/blocks/register-woo-blocks.php');
-
-            // Load the V2 product editor
-            require_once(dirname(__FILE__) . '/payment/VippsWCProductEditorV2.class.php');
-            VippsWCProductEditorV2::register_hooks();
-
-        }
-    });
-
-    /* Load recurring if the stand-alone plugin isn't active IOK 2024-09-27  */
+    /* Load support for recurring payments if the stand-alone plugin isn't active IOK 2024-09-27  */
     if (! in_array('vipps-recurring-payments-gateway-for-woocommerce/woo-vipps-recurring.php', $activeplugins)) {
         require_once(dirname(__FILE__) . "/recurring/woo-vipps-recurring.php");
     }
 }
-
-add_action ('before_woocommerce_init', function () {
- $url = sanitize_text_field($_SERVER['REQUEST_URI']);
- # This removes cookies for the wc-api callback events for the vipps plugin, to be 100% sure no sessions are restored when they ought not be IOK 2020-07-01
- # Re-added and modified IOK 2022-06-20
- if (preg_match('!(wc_gateway_vipps|vipps_shipping_details|vipps-consent-removal)!', $url) && preg_match('!\bwc-api\b!', $url)) {
-    // Disallow woo from setting any cookies for these URLs. This happens very early, so we need to do this a bit awkwardly.
-    add_filter('woocommerce_set_cookie_enabled', function ($val,$name ,$value, $expire, $secure) {
-                return false;
-    }, 999, 5);
-    // Any cookies that was previously set: Remove them.
-    foreach($_COOKIE as $key=>$value) unset($_COOKIE[$key]);
- }
-},1);
-
-// Declare our support for the HPOS feature IOK 2022-12-07
-add_action( 'before_woocommerce_init', function() {
-	if ( class_exists( '\Automattic\WooCommerce\Utilities\FeaturesUtil' ) ) {
-		\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
-	}
-});
-
-// Load the extra Vipps Checkout Shipping classes only when necessary
-add_action( 'woocommerce_shipping_init', function () {
-    if (!class_exists('VippsCheckout_Shipping_Method') && get_option('woo_vipps_checkout_activated', false)) {
-        require_once(dirname(__FILE__) . '/payment/VippsCheckoutShippingMethods.php');
-    }
-});
