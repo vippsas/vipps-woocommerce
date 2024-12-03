3261d0057ee75e9b343e513020c8e7263958f81b
Iver Odin Kvello <iver@wp-hosting.no>
2024-12-03 10:45:24 +0000

    Swedish

---
+++ languages/woo-vipps-sv_SE.l10n.php
@@ -0,0 +1,2 @@
+<?php
+return ['project-id-version'=>'Woocommerce Vipps Payment Module','report-msgid-bugs-to'=>'','pot-creation-date'=>'2024-12-02 08:56+0000','po-revision-date'=>'2024-12-03 08:08+0000','last-translator'=>'Iver Odin Kvello <iver@wp-hosting.no>','language-team'=>'Swedish','language'=>'sv_SE','plural-forms'=>'nplurals=2; plural=n != 1;','mime-version'=>'1.0','content-type'=>'text/plain; charset=UTF-8','content-transfer-encoding'=>'8bit','x-generator'=>'Loco https://localise.biz/','x-loco-version'=>'2.6.14; wp-6.7.1','messages'=>[]];
