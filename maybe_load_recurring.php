8b0989bc44b7bad13973a7c7a1afb51fcba1ca21
Iver Odin Kvello <iver@wp-hosting.no>
2024-12-04 12:12:04 +0000

    Handle deactivation of other plugin

--- maybe_load_recurring.php
+++ maybe_load_recurring.php
@@ -15,56 +15,55 @@ function woo_vipps_ajax_cron_or_rest () {
    return false;
 }
 
+/* If the standalone plugin has its "deactivate" method called, then we will need to re-activate it. IOK 2024-12-04 */
+add_action( 'deactivate_plugin', function($plugin, $network_deactivating ) {
+        if (basename($plugin) == 'woo-vipps-recurring.php') {
+            delete_option('woo_vipps_recurring_payments_activation');
+        }
+},10,2);
+
 // Load very early in plugins_loaded, since the Recurring feature will add events on precedence 10 itself.
 add_action('plugins_loaded', function () {
-   $woo_exists = function_exists('WC');
-   if (!$woo_exists) return false;
+    $woo_exists = function_exists('WC');
+    if (!$woo_exists) return false;
 
-   $vipps_recurring_exists = defined('WC_VIPPS_RECURRING_VERSION');
-   /* If the standalone plugin has been loaded, note that it has been activated, but do nothing else */
-   if ($vipps_recurring_exists) {
-       update_option('woo_vipps_recurring_payments_activation', WC_VIPPS_RECURRING_VERSION);
-       return false;
-   }
+    $vipps_recurring_exists = defined('WC_VIPPS_RECURRING_VERSION');
+    /* If the standalone plugin has been loaded, note that it has been activated, but do nothing else */
+    if ($vipps_recurring_exists) {
+        update_option('woo_vipps_recurring_payments_activation', WC_VIPPS_RECURRING_VERSION);
+        return false;
+    }
 
+    /* We will load support now if either the plugin has been activated, or if subscriptions exist */
+    $subscriptions_exist = class_exists('WC_Subscriptions');
+    $previously_activated = get_option('woo_vipps_recurring_payments_activation');
+    if (!$subscriptions_exist && !$previously_activated) {
+        return false;
+    }
 
-   /* We will load support now if either the plugin has been activated, or if subscriptions exist */
-   $subscriptions_exist = class_exists('WC_Subscriptions');
-   $previously_activated = get_option('woo_vipps_recurring_payments_activation');
-   if (!$subscriptions_exist && !$previously_activated) {
-       return false;
-   }
+    // It should now be safe to load the recurring support.
+    require_once(dirname(__FILE__) . "/recurring/recurring.php");
 
-   // It should now be safe to load the recurring support.
-   require_once(dirname(__FILE__) . "/recurring/recurring.php");
+    // We need to do the old plugin de/activation logic for the standalone plugin here:
+    // If we are here and we have not been previously activated, we should call the "activate" hook and note that we have been activated.
+    // If we are here and we were previously activated, but subscriptions are not active, we should call the *deactivate* hook and reset the database setting.
+    // This must be done fairly late, so we are going to use "wp_loaded". IOK 2024-12-04
+    if (!$previously_activated) {
+        if (!woo_vipps_ajax_cron_or_rest ()) {
+            add_action('wp_loaded', function () {
+                WC_Vipps_Recurring::get_instance()->activate();
+                update_option('woo_vipps_recurring_payments_activation', WC_VIPPS_RECURRING_VERSION);
+            });
+        }
+    } else if (!$subscriptions_exist) {
+        if (!woo_vipps_ajax_cron_or_rest ()) {
+            add_action('wp_loaded', function () {
+                WC_Vipps_Recurring::get_instance()->deactivate();
+                delete_option('woo_vipps_recurring_payments_activation');
+            });
+        }
+    } 
 
-   // We need to do the old plugin de/activation logic for the standalone plugin here:
-   // If we are here and we have not been previously activated, we should call the "activate" hook and note that we have been activated.
-   // If we are here and we were previously activated, but subscriptions are not active, we should call the *deactivate* hook and reset the database setting.
-   // This must be done fairly late, so we are going to use "wp_loaded". IOK 2024-12-04
-   if (!$previously_activated) {
-       if (!woo_vipps_ajax_cron_or_rest ()) {
-           error_log("We have loaded the code but not previously activated the plugin.");
-           add_action('wp_loaded', function () {
-               error_log("wp loaded. Activating recurring");
-               WC_Vipps_Recurring::get_instance()->activate();
-               update_option('woo_vipps_recurring_payments_activation', WC_VIPPS_RECURRING_VERSION);
-           });
-       }
-   } else if (!$subscriptions_exist) {
-       if (!woo_vipps_ajax_cron_or_rest ()) {
-           error_log("We have loaded the code but woo subscriptions are not present.");
-           add_action('wp_loaded', function () {
-               error_log("wp loaded. dectivating.");
-               WC_Vipps_Recurring::get_instance()->deactivate();
-               delete_option('woo_vipps_recurring_payments_activation');
-           });
-       }
-   } else {
-       if (!woo_vipps_ajax_cron_or_rest ()) {
-           error_log("We have loaded the code, but we are already activated");
-       }
-   }
 
 
 }, 1);
