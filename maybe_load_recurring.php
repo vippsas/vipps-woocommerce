fa59f3070188a415c104f0de172dce4463aa4b73
Iver Odin Kvello <iver@wp-hosting.no>
2024-12-04 09:09:45 +0000

    Activation

--- maybe_load_recurring.php
+++ maybe_load_recurring.php
@@ -24,20 +24,43 @@ function woo_vipps_ajax_cron_or_rest () {
 // Load very early in plugins_loaded, since the Recurring feature will add events on precedence 10 itself.
 add_action('plugins_loaded', function () {
    $woo_exists = function_exists('WC');
-   $vipps_recurring_exists = defined('WC_VIPPS_RECURRING_VERSION');
+   if (!$woo_exists) return false;
 
-   /* If no woo, or recurring is already loaded, we don't do anything */
-   if (!$woo_exists || $vipps_recurring_exists) {
+   $vipps_recurring_exists = defined('WC_VIPPS_RECURRING_VERSION');
+   /* If the standalone plugin has been loaded, note that it has been activated, but do nothing else */
+   if ($vipps_recurring_exists) {
+       update_option('woo_vipps_recurring_payments_activation', WC_VIPPS_RECURRING_VERSION);
        return false;
    }
 
-   /* We will load support */
+
+   /* We will load support now if either the plugin has been activated, or if subscriptions exist */
    $subscriptions_exist = class_exists('WC_Subscriptions');
-   $previously_activated = get_option('woo_vipps_recurring_payments');
+   $previously_activated = get_option('woo_vipps_recurring_payments_activation');
+
+error_log("Previously activated: $previously_activated");
+
    if ($subscriptions_exist || $previously_activated) {
-       error_log("Loading recurring support");
+error_log("Loading recurring support");
        require_once(dirname(__FILE__) . "/recurring/recurring.php");
    }
+   // We need to do the old plugin de/activation logic for the standalone plugin here:
+   // If we are here and we have not been previously activated, we should call the "activate" hook and note that we have been activated.
+   // If we are here and we were previously activated, but subscriptions are not active, we should call the *deactivate* hook and reset the database setting.
+   if (!$previously_activated) {
+       if (!woo_vipps_ajax_cron_or_rest ()) {
+           error_log("We have loaded the code but not previously activated the plugin.");
+       }
+   } else if (!$subscriptions_exists) {
+       if (!woo_vipps_ajax_cron_or_rest ()) {
+           error_log("We have loaded the code but woo subscriptions are not present.");
+       }
+   } else {
+       if (!woo_vipps_ajax_cron_or_rest ()) {
+           error_log("We have loaded the code, but we are already activated");
+       }
+   }
+
 
 }, 1);
 
