783c438153f67c41b325d87d1837ee27ebf33abe
Iver Odin Kvello <iver@wp-hosting.no>
2025-01-06 10:37:23 +0000

    After change of translation domain

--- recurring/assets/build/checkout.asset.php
+++ recurring/assets/build/checkout.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'a65bf7a53eb74b650f0b');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'd749b57a627077b59f7d');
--- recurring/assets/build/main.asset.php
+++ recurring/assets/build/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '39e4ea57951c2094e303');
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '08dfe8888b56a3b795b0');
