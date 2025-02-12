65b0ef4902dfaef96b5fd82b68c1c6a9bc7eb26d
Lasse Pladsen <lasse.pladsen@hotmail.com>
2025-02-12 12:25:05 +0100

    title

--- payment/WC_Gateway_Vipps.class.php
+++ payment/WC_Gateway_Vipps.class.php
@@ -959,7 +959,7 @@ class WC_Gateway_Vipps extends WC_Payment_Gateway {
                     ),
 
                 'checkout_options_wizard' => array(
-                    'title' => Vipps::CheckoutName(),
+                    'title' => sprintf(__('Get started with %1$s', 'woo-vipps'), Vipps::CheckoutName()),
                     'type'  => 'title',
                     'class' => 'tab',
                     'description' => sprintf(__('%1$s is a service from %2$s, which allows you to replace the usual WooCommerce checkout page with a super simple checkout screen, where your customers can pay with Vipps, Visa, and MasterCard!', 'woo-vipps'), Vipps::CheckoutName(), Vipps::CompanyName()),
@@ -974,7 +974,7 @@ class WC_Gateway_Vipps extends WC_Payment_Gateway {
                     ),
 
                 'vipps_checkout_enabled_wizard' => array(
-                    'title'       => sprintf(__('%1$s screen', 'woo-vipps'), Vipps::CheckoutName()),
+                    'title'       => Vipps::CheckoutName(),
                     'label'       => sprintf(__('Yes, I want to start using %1$s', 'woo-vipps'), Vipps::CheckoutName()),
                     'type'        => 'checkbox',
                     'description' => sprintf(__('If activated, this will <strong>replace</strong> the standard Woo checkout screen with %1$s, providing easy checkout using %1$s or credit card, with no need to type in addresses.', 'woo-vipps'), Vipps::CheckoutName()),
