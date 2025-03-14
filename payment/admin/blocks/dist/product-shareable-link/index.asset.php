b3d03a554f8d5fee8b642c496443a5ffec13c54e
Iver Odin Kvello <iver@wp-hosting.no>
2025-03-14 08:49:18 +0000

    Rebuild

--- payment/admin/blocks/dist/product-shareable-link/index.asset.php
+++ payment/admin/blocks/dist/product-shareable-link/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wc-block-templates', 'wc-components', 'wc-product-editor', 'wp-components', 'wp-i18n'), 'version' => '0257522de1488f990e86');
+<?php return array('dependencies' => array('react', 'wc-block-templates', 'wc-components', 'wc-product-editor', 'wp-components', 'wp-i18n'), 'version' => '97c0ff1ba09d3bcb3633');
