5ccd1f5a8b68eee153d61a7814f9f12a7f306c78
Lasse Pladsen <lasse.pladsen@hotmail.com>
2025-09-25 11:34:02 +0200

    add checkout status poll after expire

--- payment/VippsCheckout.class.php
+++ payment/VippsCheckout.class.php
@@ -1254,12 +1254,14 @@ jQuery(document).ready(function () {
             // Expire the Checkout session. LP 2025-09-25
             $expire_result = $this->gateway()->api->checkout_expire_session($order);
             $this->log(sprintf(__('Expiring the Checkout session because the order was cancelled: %1$s.', 'woo-vipps'), $order->get_id()), 'debug');
-            $this->log(sprintf(__('Checkout expire post returned: %1$s', 'woo-vipps'), print_r($expire_result, true), 'debug'); // TODO: remove this logging line after expire settles ok? LP 2025-09-25
+            $this->log(sprintf(__('Checkout expire post returned: %1$s', 'woo-vipps'), print_r($expire_result, true)), 'debug'); // TODO: remove this logging line after expire settles ok? LP 2025-09-25
 
             // Also mark for deletion and remove stored session
             $order->delete_meta_data('_vipps_checkout_session');
             $order->update_meta_data('_vipps_delendum',1);
             $order->save();
+
+            $this->log(sprintf(__('Checkout status for order %1$s is %2$s', 'woo-vipps'), $order->get_id(), print_r($this->get_vipps_checkout_status($order), true)), 'debug'); // FIXME: delete. For debug. LP 2025-09-25
 
         }
     }
