375b6b533c47855f1f95c48e7b2f4f92bb692843
Iver Odin Kvello <iver@wp-hosting.no>
2025-09-30 08:21:01 +0000

    5.0.5

--- woo-vipps.php
+++ woo-vipps.php
@@ -7,14 +7,14 @@
    Author URI: https://www.wp-hosting.no/
    Text-domain: woo-vipps
    Domain Path: /languages
-   Version: 5.0.4
-   Stable tag: 5.0.4
+   Version: 5.0.5
+   Stable tag: 5.0.5
    Requires at least: 6.2
    Tested up to: 6.8.2
    Requires PHP: 7.4
    Requires Plugins: woocommerce
    WC requires at least: 3.3.4
-   WC tested up to: 10.2.0
+   WC tested up to: 10.2.2
 
    License: MIT
    License URI: https://choosealicense.com/licenses/mit/
