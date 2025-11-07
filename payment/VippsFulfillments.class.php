e66787cd04f436dca97b729ebdaa3eb706a8c804
Lasse Pladsen <lasse.pladsen@hotmail.com>
2025-11-07 07:50:37 +0000

    Feat: stop fulfillment processing early if nothing left to capture in order. Refactor & comments

--- payment/VippsFulfillments.class.php
+++ payment/VippsFulfillments.class.php
@@ -62,10 +62,10 @@ class VippsFulfillments {
      *  This hook will also run on fulfillments edits, after the hook '...before_update'
      *  therefore here we loop over all fulfillment items and stop if the new fulfill sum is less than what is already captured at Vipps MobilePay. LP 2025-10-31
      */
-    public function woocommerce_fulfillment_before_fulfill($new_fulfillment) {
+    public function woocommerce_fulfillment_before_fulfill($fulfillment) {
         error_log("LP running woocommerce_fulfillment_before_fulfill");
 
-        $order = $new_fulfillment->get_order();
+        $order = $fulfillment->get_order();
         if (!$order) {
             $this->fulfillment_fail(__('Something went wrong, could not find order', 'woo-vipps'));
         }
@@ -73,27 +73,31 @@ class VippsFulfillments {
         // This is handled in gateway->capture_payment(), but might as well return early here if not Vipps MobilePay. LP 2025-10-27
         $pm = $order->get_payment_method();
         if ($pm != 'vipps') {
-            return $new_fulfillment;
+            return $fulfillment;
+        }
+
+        // Don't process anything further if the order is entirely captured already. LP 2025-11-07
+        if(intval(!$order->get_meta('_vipps_capture_remaining'))) {
+            return $fulfillment;
         }
 
         $currency = $order->get_currency();
 
         $to_capture = 0;
         $item_capture_table = []; // store new capture sum for each item for the purpose of updating metas when we know capture success. LP 2025-10-31
-        foreach ($new_fulfillment->get_items() as $item) {
+        foreach ($fulfillment->get_items() as $item) {
             $item_id = $item['item_id'];
             error_log('LP before_fulfill item_id: ' . print_r($item_id, true));
-            $item_quantity = $item['qty'];
+            $fulfill_quantity = $item['qty'];
             $order_item = $order->get_item($item_id);
             if (!$order_item) {
                 $this->fulfillment_fail('Something went wrong, could not find fulfillment item'); // how did this happen
             }
             error_log('LP before_fulfill item_name: ' . print_r($order_item->get_name(), true));
-            $item_sum = $order->get_item_total($order_item, true, false) * $item_quantity * 100;
-            error_log('LP item_sum: ' . print_r($item_sum, true));
+            $item_sum = (int) $order->get_item_total($order_item, true, false) * $fulfill_quantity * 100;
+            error_log('LP before_fulfill item_sum: ' . print_r($item_sum, true));
 
-            // Stop here and give the user a fail message if they try to remove already-captured amounts for this order item.
-            // This is for fulfillment edits. LP 2025-10-31
+            // Stop here and give the user a fail message if they try to remove already-captured amounts for this order item (fulfillment edits). LP 2025-10-31
             $item_captured_sum = intval($order_item->get_meta('_vipps_item_captured'));
             if ($item_sum < $item_captured_sum) {
                 /* translators: %1 = item product name, %2 = company name */
@@ -104,11 +108,10 @@ class VippsFulfillments {
             $to_capture += $item_sum;
         }
 
-
         error_log('LP before_fulfill to_capture ' . print_r($to_capture, true));
-        if ($to_capture == 0) {
-            error_log("LP before_fulfill new capture is zero, not sending anything to capture");
-            return $new_fulfillment;
+        if ($to_capture <= 0) {
+            error_log("LP before_fulfill new capture is zero or below, not sending anything to capture");
+            return $fulfillment;
         }
 
         $ok = $this->gateway->capture_payment($order, $to_capture);
@@ -128,7 +131,7 @@ class VippsFulfillments {
             }
         }
 
-        return $new_fulfillment;
+        return $fulfillment;
     }
 
     /** Returns failure message to fulfillment admin interface by throwing FulfillmentException. LP 2025-10-15 */
