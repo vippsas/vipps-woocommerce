3c99f05a04442bfe38152f4952c020bb62dfd000
Marcus Dahl <marcus.dahl@everyday.no>
2026-01-06 11:27:30 +0100

    Make sure to use 100% of the width available to us in the Recurring Checkout

--- recurring/assets/build/checkout.asset.php
+++ recurring/assets/build/checkout.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => 'd749b57a627077b59f7d');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '9ed2eb41cd992531a0ac');
