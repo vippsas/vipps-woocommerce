e40090babdb2d0c556f726ab86996b4c3a277a75
Marcus Dahl <marcus.dahl@everyday.no>
2026-01-27 13:05:35 +0100

    Set language correctly in the recurring Checkout

--- recurring/assets/build/checkout.asset.php
+++ recurring/assets/build/checkout.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '9ed2eb41cd992531a0ac');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-element', 'wp-i18n'), 'version' => '5490f2a514335ed5ae48');
