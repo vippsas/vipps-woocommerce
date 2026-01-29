53f4dba42b642ac1e4ec1a294b2286f503e6a66f
Lasse Pladsen <lasse.pladsen@hotmail.com>
2026-01-29 12:52:39 +0100

    formatting

--- payment/src/Blocks/src/buy-now/hooks/useIsInsideCart.js
+++ payment/src/Blocks/src/buy-now/hooks/useIsInsideCart.js
@@ -1,12 +1,11 @@
 import { useSelect } from '@wordpress/data';
 import { store as blockEditorStore } from '@wordpress/block-editor';
 
-const CART_MINICART_BLOCKNAMES = new Set(
-	[
-	"woocommerce/cart",
-	"woocommerce/cart-contents",
-	"woocommerce/mini-cart",
-	"woocommerce/mini-cart-contents",
+const CART_MINICART_BLOCKNAMES = new Set([
+	'woocommerce/cart',
+	'woocommerce/cart-contents',
+	'woocommerce/mini-cart',
+	'woocommerce/mini-cart-contents',
 ]);
 
 /** Whether the client is a descendent of a cart or mini-cart block. LP 2026-01-29 */
@@ -15,7 +14,9 @@ const useIsInsideCart = (clientId) =>
 		(select) => {
 			const { getBlockParents, getBlockName } = select(blockEditorStore);
 			const parents = getBlockParents(clientId);
-			return parents.some((id) => CART_MINICART_BLOCKNAMES.has(getBlockName(id)));
+			return parents.some((id) =>
+				CART_MINICART_BLOCKNAMES.has(getBlockName(id))
+			);
 		},
 		[clientId]
 	);
