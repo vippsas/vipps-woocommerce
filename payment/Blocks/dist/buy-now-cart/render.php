91adf0a94e7cd7bde6799fc15ec70e4ddb3de043
Lasse Pladsen <lasse.pladsen@hotmail.com>
2026-02-10 12:43:43 +0100

    remove comment

--- payment/Blocks/dist/buy-now-cart/render.php
+++ payment/Blocks/dist/buy-now-cart/render.php
@@ -9,7 +9,6 @@ $wrapper_attributes = get_block_wrapper_attributes([
 echo "<div $wrapper_attributes>";
 // inverting the condition with "!" doesn't work, even though the docs use it (although different example), so thats why we use "cart_hide_express" instead of a normal "cart_show_express". (https://developer.wordpress.org/block-editor/reference-guides/interactivity-api/api-reference/) LP 2026-02-10
 echo '<span data-wp-bind--hidden="woocommerce::state.cart.extensions.woo-vipps.cart_hide_express">';
-// Vipps::instance()->minicart_express_checkout_button();
 Vipps::instance()->cart_express_checkout_button_html(true); // always print the html, the directive 'data-wp-bind--hidden' above will show or hide it. LP 2026-02-10
 echo '</span>';
 echo "</div>";
