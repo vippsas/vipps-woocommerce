3c827b0f12652a57605d0e2f69b082ffce990aa3
Lasse Pladsen <lasse.pladsen@hotmail.com>
2026-02-10 10:04:34 +0100

    testing interactivity api

--- payment/Blocks/dist/buy-now-cart/render.php
+++ payment/Blocks/dist/buy-now-cart/render.php
@@ -1,2 +1,14 @@
 <?php
-echo "<div class='wp-block-button wc-block-components-product-button wc-block-button-vipps'>" . Vipps::instance()->minicart_express_checkout_button() . "</div>";
+if (! defined('ABSPATH')) {
+    exit;
+}
+
+error_log('LP rendering the block!');
+
+$wrapper_attributes = get_block_wrapper_attributes([
+    'class' => 'wp-block-button wc-block-components-product-button wc-block-button-vipps',
+    'data-wp-bind--hidden' => '!woocommerce::state.cart.extensions.woo_vipps.cart_supports_express'
+]);
+echo "<div $wrapper_attributes>";
+Vipps::instance()->minicart_express_checkout_button();
+echo "</div>";
--- payment/src/Blocks/src/buy-now-cart/edit.tsx
+++ payment/src/Blocks/src/buy-now-cart/edit.tsx
@@ -12,6 +12,7 @@ export default function Edit() {
 					'wp-block-button wc-block-components-product-button wc-block-button-vipps',
 			})}
 		>
+			BUYNOW
 			{/* The buy-now button. LP 2026-01-19 */}
 			<a
 				className="single-product button vipps-buy-now wp-block-button__link"
