846f30bf823278b583aa416a917e3cb01077d174
Lasse Pladsen <lasse.pladsen@hotmail.com>
2026-06-17 11:55:50 +0200

    v6.0.4

--- woo-vipps.php
+++ woo-vipps.php
@@ -7,8 +7,8 @@
    Author URI: https://www.wp-hosting.no/
    Text-domain: woo-vipps
    Domain Path: /languages
-   Version: 6.0.3
-   Stable tag: 6.0.3
+   Version: 6.0.4
+   Stable tag: 6.0.4
    Requires at least: 6.3
    Tested up to: 7.0.0
    Requires PHP: 8.0
