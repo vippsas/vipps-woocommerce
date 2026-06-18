0c39d3d2f618776dc3d7e145ff1d0fac5794f0cf
Marcus Dahl <marcus.dahl@everyday.no>
2026-06-18 12:33:46 +0200

    Allow disabling retry days and add processing mode for charges

--- recurring/recurring.php
+++ recurring/recurring.php
@@ -3,7 +3,7 @@ defined( 'ABSPATH' ) || exit;
 
 // phpcs:disable WordPress.Files.FileName
 
-define( 'WC_VIPPS_RECURRING_VERSION', '2.2.13' );
+define( 'WC_VIPPS_RECURRING_VERSION', '2.2.14' );
 
 /**
  * Polyfills
