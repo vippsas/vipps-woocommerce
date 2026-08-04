c8881cf8f3c759d332fb9af940f9b9e2f737d016
Iver Odin Kvello <iver@wp-hosting.no>
2026-08-04 10:18:48 +0000

    Fix button for exprss

--- payment/Vipps.class.php
+++ payment/Vipps.class.php
@@ -5456,7 +5456,7 @@ else:
             $content .= $termsHTML;
             $content .= apply_filters('woo_vipps_express_checkout_validation_elements', '');
             $title = sprintf(__('Buy now with %1$s!', 'woo-vipps'), $this->get_payment_method_name());
-            $content .= "<div class='vipps_buy_now_wrapper noloop'><a href='#' id='do-express-checkout' class='button vipps-express-checkout' title='$title'><img alt='$title' border=0 src='$buttonimgurl'></a></div>";
+            $content .= "<div class='vipps_buy_now_wrapper noloop'><a href='#' id='do-express-checkout' class='button vipps-express-checkout' title='$title'>$buttonimgurl</a></div>";
             $content .= "<div id='vipps-status-message'></div>";
             $this->fakepage(sprintf(__('%1$s Express Checkout','woo-vipps'), $this->get_payment_method_name()), $content);
             return;
