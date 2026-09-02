a6b4f6c4bb6f2a6c3f896d656b8297a793447567
Iver Odin Kvello <iver@wp-hosting.no>
2026-09-02 14:03:20 +0000

    Work in progress

--- payment/Vipps.class.php
+++ payment/Vipps.class.php
@@ -4695,6 +4695,9 @@ else:
         }
         $result = ['ok' => 0, 'msg'=>'', 'orderid'=>0, 'url'=>''];
 
+
+error_log("args are " . print_r($args, true));
+
         // We receive the varid, prodid, sku and quantity directly. One of these. The sku is the dominant one. IOK 2026-08-27
         $varid = intval($args['variation_id'] ?? 0);
         $prodid = intval($args['product_id'] ?? 0);
@@ -4744,27 +4747,48 @@ else:
             WC()->cart->get_cart_from_session();
         }
 
-        // Now based on this, compute a hash. We'll store that in the session for ~5 minutes, and if there is a previous order with this
-        // hash *in session* we'll ask the user their intent.
-        // FIXME abstract this and stuff it deeper in the system.
-        $last_express_purchase_hash = WC()->session->get('woo_vipps_last_express');
-        $last_express_the_same = false;
+        // Try to avoid re-purchasing the same order repeatedly. IOK 2026-09-02
         $current_hash = md5("$prodid:$varid:$quantity");
-        if ($last_express_purchase_hash) {
-           list($hash, $stamp) = explode(":", $last_express_purchase_hash);
-           $cutoff = $stamp + apply_filters('woo_vipps_recent_order_cutoff', (3*60));
-           if ($hash == $current_hash && (time() <= $cutoff )) {
-              error_log("This is the same purchase as earlier");
-              $result = [ 'ok' => 0, 'msg' => 'Repurchase', 'orderid'=>0, 'url'=>'']; // FIXME here we should return a restart thing.
-           }
-        } else {
+
+        $confirmation = (bool) intval(($others['confirmed'] ?? 0));
+        $require_confirmation = false;
+        if (!$confirmation) {
+            // Now based on this, compute a hash. We'll store that in the session for ~5 minutes, and if there is a previous order with this
+            // hash *in session* we'll ask the user their intent.
+            // FIXME abstract this and stuff it deeper in the system.
+            $last_express_purchase_hash = WC()->session->get('woo_vipps_last_express');
+            $last_express_the_same = false;
+            if ($last_express_purchase_hash) {
+                list($hash, $stamp) = explode(":", $last_express_purchase_hash);
+                $cutoff = $stamp + apply_filters('woo_vipps_recent_order_cutoff', (3*60));
+                if ($hash == $current_hash && (time() <= $cutoff )) {
+                    $header = __("Are you sure?",'woo-vipps');
+                    $body = __("You recently completed an order with exactly the same products as you are buying now. There should be an email in your inbox from the previous purchase. Are you sure you want to order again?",'woo-vipps');
+                    $html = "<h1>$header</h1><p>$body</p>";
+
+                    $result = [ 'ok' => 2, 'msg' => 'confirm', 'html'=>$html, 'orderid'=>0, 'url'=>'']; 
+                    $require_confirmation = true;
+                }
+            }
+        }
+
+        // For testing.
+        if (! $confirmation) {
+            $header = __("Are you sure?",'woo-vipps');
+            $body = __("You recently completed an order with exactly the same products as you are buying now. There should be an email in your inbox from the previous purchase. Are you sure you want to order again?",'woo-vipps');
+            $html = "<h1>$header</h1><p>$body</p>";
+            $result = [ 'ok' => 2, 'msg' => 'confirm', 'html'=>$html, 'orderid'=>0, 'url'=>'']; 
+            $require_confirmation = true;
+        }
+
+        if (!$require_confirmation) {
             // Basically always return 200 after this, and always return an object with an 'ok' and a 'msg' value, possibly 'orderid' and 'url'.
             $result = $this->really_do_single_product_express_checkout($prodid, $varid, $sku, $quantity, $variations);
             // And if we're going to express now so let's note the order. IOK 2026-08-27. Now this assumes success, but *basically* I think this is ok.
             // We'll reset it on order failure I think. IOK 2026-08-20 FIXME
             if ($result['ok']) {
-                WC()->session->set('woo_vipps_last_express', "$current_hash:" . time());
-                WC()->session->save_data();
+            WC()->session->set('woo_vipps_last_express', "$current_hash:" . time());
+            WC()->session->save_data();
             }
         }
 
