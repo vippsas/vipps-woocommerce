fbedf7a2bb8b445e74f24f5489a2893e55e6bdd2
Lasse Pladsen <lasse.pladsen@hotmail.com>
2026-09-25 10:13:36 +0200

    only show rest of express options if its enabled at all

--- payment/src/admin/settings/src/components/admin-settings/express-options-tab.tsx
+++ payment/src/admin/settings/src/components/admin-settings/express-options-tab.tsx
@@ -1,5 +1,7 @@
 import { gettext } from '../../lib/wp-data';
+import { useWP } from '../../wp-options-provider';
 import { Collapsible } from '../collapsible';
+import { truthToBool } from '../form-elements';
 import { CheckboxFormField, SelectFormField } from '../options-form-fields';
 
 /**
@@ -8,6 +10,12 @@ import { CheckboxFormField, SelectFormField } from '../options-form-fields';
  * @returns The rendered express options tab.
  */
 export function AdminSettingsExpressOptionsTab(): JSX.Element {
+  const { getOption } = useWP();
+  const expressEnabled = truthToBool(getOption('cartexpress'))
+    || truthToBool(getOption('express_show_in_checkout'))
+    || truthToBool(getOption('singleproductexpressarchives'))
+    || getOption('singleproductexpress') !== 'none';
+
   return (
     <div>
       <p className="vipps-mobilepay-react-tab-description">{gettext('express_options.description')}</p>
@@ -48,59 +56,62 @@ export function AdminSettingsExpressOptionsTab(): JSX.Element {
         ]}
       />
 
-      {/* Shipping section */ }
-      <Collapsible title={gettext('express_shipping_section.title')}>
-        {/* Renders a checkbox to enable whether or not the users should always be asked for an address */}
-        <CheckboxFormField
-          name="expresscheckout_always_address"
-          titleKey="expresscheckout_always_address.title"
-          labelKey="expresscheckout_always_address.label"
-          descriptionKey="expresscheckout_always_address.description"
-        />
+      {/* Only show the rest of the options if express is actually enabled (the options above). LP 2026-09-25 */}
+      {expressEnabled && (<>
+        {/* Shipping section */ }
+        <Collapsible title={gettext('express_shipping_section.title')}>
+          {/* Renders a checkbox to enable whether or not the users should always be asked for an address */}
+          <CheckboxFormField
+            name="expresscheckout_always_address"
+            titleKey="expresscheckout_always_address.title"
+            labelKey="expresscheckout_always_address.label"
+            descriptionKey="expresscheckout_always_address.description"
+          />
 
-        {/* Renders a checkbox to enable static shipping for Express Checkout */}
-        <CheckboxFormField
-          name="enablestaticshipping"
-          titleKey="enablestaticshipping.title"
-          labelKey="enablestaticshipping.label"
-          descriptionKey="enablestaticshipping.description"
-        />
-      </Collapsible>
+          {/* Renders a checkbox to enable static shipping for Express Checkout */}
+          <CheckboxFormField
+            name="enablestaticshipping"
+            titleKey="enablestaticshipping.title"
+            labelKey="enablestaticshipping.label"
+            descriptionKey="enablestaticshipping.description"
+          />
+        </Collapsible>
 
-      {/* Advanced section */ }
-      <Collapsible title={gettext('express_advanced_section.title')}>
-        {/* Renders a checkbox to enable whether or not users should be asked if they've read the store's terms and conditions */}
-        <CheckboxFormField
-          name="expresscheckout_termscheckbox"
-          titleKey="expresscheckout_termscheckbox.title"
-          labelKey="expresscheckout_termscheckbox.label"
-          descriptionKey="expresscheckout_termscheckbox.description"
-        />
+        {/* Advanced section */ }
+        <Collapsible title={gettext('express_advanced_section.title')}>
+          {/* Renders a checkbox to enable whether or not users should be asked if they've read the store's terms and conditions */}
+          <CheckboxFormField
+            name="expresscheckout_termscheckbox"
+            titleKey="expresscheckout_termscheckbox.title"
+            labelKey="expresscheckout_termscheckbox.label"
+            descriptionKey="expresscheckout_termscheckbox.description"
+          />
 
-        {/* Renders a checkbox to enable whether or not new users should be created when using Express Checkout */}
-        <CheckboxFormField
-          name="expresscreateuser"
-          titleKey="expresscreateuser.title"
-          labelKey="expresscreateuser.label"
-          descriptionKey="expresscreateuser.description"
-        />
+          {/* Renders a checkbox to enable whether or not new users should be created when using Express Checkout */}
+          <CheckboxFormField
+            name="expresscreateuser"
+            titleKey="expresscreateuser.title"
+            labelKey="expresscreateuser.label"
+            descriptionKey="expresscreateuser.description"
+          />
 
-        {/* Renders a checkbox to enable whether or not failed Express Checkout orders should be deleted */}
-        <CheckboxFormField
-          name="deletefailedexpressorders"
-          titleKey="deletefailedexpressorders.title"
-          labelKey="deletefailedexpressorders.label"
-          descriptionKey="deletefailedexpressorders.description"
-        />
+          {/* Renders a checkbox to enable whether or not failed Express Checkout orders should be deleted */}
+          <CheckboxFormField
+            name="deletefailedexpressorders"
+            titleKey="deletefailedexpressorders.title"
+            labelKey="deletefailedexpressorders.label"
+            descriptionKey="deletefailedexpressorders.description"
+          />
 
-        {/* Renders a checkbox to enable compatibility mode for the "Buy now" button */}
-        <CheckboxFormField
-          name="singleproductbuynowcompatmode"
-          titleKey="singleproductbuynowcompatmode.title"
-          labelKey="singleproductbuynowcompatmode.label"
-          descriptionKey="singleproductbuynowcompatmode.description"
-        />
-      </Collapsible>
+          {/* Renders a checkbox to enable compatibility mode for the "Buy now" button */}
+          <CheckboxFormField
+            name="singleproductbuynowcompatmode"
+            titleKey="singleproductbuynowcompatmode.title"
+            labelKey="singleproductbuynowcompatmode.label"
+            descriptionKey="singleproductbuynowcompatmode.description"
+          />
+        </Collapsible>
+      </>)}
     </div>
   );
 }
